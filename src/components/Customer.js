90041df90ab3ff9cf53fcbed13ff6908ec7279ce
WoolinChoi <woolin06@naver.com>
2019-10-23 15:04:31 +0900

    Create Customer.js

---
+++ src/components/Customer.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+
+class Customer extends Component{
+    render(){
+        return(
+            <div>
+                <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name}/>
+                <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job}/>
+            </div>
+        )
+    }
+}
+
+// 사용자의 사진과 이름, id
+class CustomerProfile extends Component{
+    render(){
+        return(
+            <div>
+                <img src={this.props.image} alt="profile"/>
+                <h2>{this.props.name}({this.props.id})</h2>
+            </div>
+        )
+    }
+}
+
+// 사용자의 남은 데이터
+class CustomerInfo extends Component{
+    render(){
+        return(
+            <div>
+                <h2>{this.props.name}</h2>
+                <p>{this.props.birthday}</p>
+                <p>{this.props.gender}</p>
+                <p>{this.props.job}</p>
+            </div>
+        )
+    }
+}
+
+export default Customer;
